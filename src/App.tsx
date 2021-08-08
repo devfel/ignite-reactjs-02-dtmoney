d45d30796fb48832e421ea714a319da2f922e381
Felizardo <devfelizardo@gmail.com>
2021-08-07 22:32:46 -0300

    adding favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: #8257e6;
+`;
+
 export function App() {
   return (
     <div className="App">
+      <Title>Testing Styled Components</Title>
       <h1>Welcome to DT Money</h1>
     </div>
   );
