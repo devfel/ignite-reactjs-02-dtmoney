f47afd01843b4a34bc6ec1335e17877b280a69e7
Felizardo <devfelizardo@gmail.com>
2021-08-07 23:59:33 -0300

    creating and styling header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import styled from "styled-components";
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-`;
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <Title>Testing Styled Components</Title>
-      <h1>Welcome to DT Money</h1>
-    </div>
+    <>
+      <GlobalStyle />
+      <Header />
+    </>
   );
 }
