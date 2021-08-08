3ce951b29df28fa2f257bcc69366764612ebe7b9
Felizardo <devfelizardo@gmail.com>
2021-08-08 15:21:24 -0300

    creating few elements for transaction table

---
+++ src/components/TransactionTable/index.tsx
@@ -0,0 +1,33 @@
+import { Container } from "./styles";
+
+export function TransactionTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Value</th>
+            <th>Category</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Website Development</td>
+            <td>1000.00</td>
+            <td>Job</td>
+            <td>13/01/2020</td>
+          </tr>
+          <tr>
+            <td>Water</td>
+            <td>100.00</td>
+            <td>Bill</td>
+            <td>15/01/2020</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
