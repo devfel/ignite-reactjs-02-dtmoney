c2fba6adf300c27099e03724727e5a09f39dbc74
Felizardo <devfelizardo@gmail.com>
2021-08-08 15:30:52 -0300

    styling components of transaction table

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -16,13 +16,13 @@ export function TransactionTable() {
         <tbody>
           <tr>
             <td>Website Development</td>
-            <td>1000.00</td>
+            <td className="deposit">1000.00</td>
             <td>Job</td>
             <td>13/01/2020</td>
           </tr>
           <tr>
             <td>Water</td>
-            <td>100.00</td>
+            <td className="withdraw">- 100.00</td>
             <td>Bill</td>
             <td>15/01/2020</td>
           </tr>
