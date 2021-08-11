16ce5e6e3bb3b31a89618b17dcccc607982234e2
Felizardo <devfelizardo@gmail.com>
2021-08-11 02:59:24 -0300

    final summary data calculated and showed

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -10,6 +10,24 @@ export function Summary() {
   const { transactions } = useContext(TransactionsContext);
   console.log(transactions);
 
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "deposit") {
+        acc.deposits += transaction.amount;
+        acc.totals += transaction.amount;
+      } else {
+        acc.withdraws += transaction.amount;
+        acc.totals -= transaction.amount;
+      }
+      return acc;
+    },
+    {
+      deposits: 0,
+      withdraws: 0,
+      totals: 0,
+    }
+  );
+
   return (
     <Container>
       {/* Old way of doing context consumer instead of useContext */}
@@ -24,21 +42,21 @@ export function Summary() {
           <p>Incomes</p>
           <img src={IncomeImg} alt="Incomes" />
         </header>
-        <strong>R$1000.00</strong>
+        <strong>{new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(summary.deposits)}</strong>
       </div>
       <div>
         <header>
           <p>Outcomes</p>
           <img src={OutcomeImg} alt="Outcomes" />
         </header>
-        <strong>- R$300.00</strong>
+        <strong>- {new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(summary.withdraws)}</strong>
       </div>
       <div className="highlight-background">
         <header>
           <p>Total</p>
           <img src={TotalImg} alt="Total" />
         </header>
-        <strong>R$700.00</strong>
+        <strong>{new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(summary.totals)}</strong>
       </div>
     </Container>
   );
